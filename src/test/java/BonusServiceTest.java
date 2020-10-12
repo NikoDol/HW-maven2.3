123197db84410272b81629d4a3a378399a9142bf
NikoDol <69104440+NikoDol@users.noreply.github.com>
2020-10-12 09:52:19 +0500

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,19 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTest {
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateRegisteredUnderLimit() {
-        BonusService service = new BonusService();
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 300;
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
     void shouldCalculateUnregisteredUnderLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
@@ -47,32 +34,6 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
-        long expected = 10;
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateUnregisteredBeforeLimit() {
-        BonusService service = new BonusService();
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = false;
-        long expected = 2;
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateRegisteredBeforeLimit() {
-        BonusService service = new BonusService();
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
         long expected = 10;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
