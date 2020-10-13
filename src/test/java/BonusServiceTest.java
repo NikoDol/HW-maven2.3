7f2bb54a26c80720bf65b12617fd393d2495e5d4
NikoDol <69104440+NikoDol@users.noreply.github.com>
2020-10-13 08:25:31 +0500

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -5,39 +5,30 @@ class BonusServiceTest {
     @org.junit.jupiter.api.Test
     void shouldCalculateUnregisteredUnderLimit() {
         BonusService service = new BonusService();
-        // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
         long expected = 30;
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
     void shouldCalculateRegistered() {
         BonusService service = new BonusService();
-        // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = true;
         long expected = 30;
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
     void shouldCalculateUnregistered() {
         BonusService service = new BonusService();
-        // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
         long expected = 10;
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
-        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 }
