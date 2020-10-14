256359dfddc5447d0c48ada8a69c299ea80075f4
NikoDol <69104440+NikoDol@users.noreply.github.com>
2020-10-14 19:54:19 +0500

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,6 +3,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTest {
 
     @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 300;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
     void shouldCalculateUnregisteredUnderLimit() {
         BonusService service = new BonusService();
         long amount = 1000_60;
@@ -27,6 +37,26 @@ class BonusServiceTest {
         BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateUnregisteredBeforeLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 2;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredBeforeLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
         long expected = 10;
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
